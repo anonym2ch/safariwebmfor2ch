cf33936c15a7ffec39ed4227c0e1add5b5ceeb60
anonym2ch <35051996+anonym2ch@users.noreply.github.com>
2018-01-05 16:25:02 +0300

    Update SafariWebmFor2ch.user.js

--- SafariWebmFor2ch.user.js
+++ SafariWebmFor2ch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Safari Webm viewer for 2ch.hk
+// @name         Safari webm viewer for 2ch.hk
 // @namespace    safari-webm-viewer-for-2ch.hk
-// @version      0.2
+// @version      0.3
 // @description  Thanks for the idea to https://github.com/C9HDN
 // @author       Anonym
 // @match        https://2ch.hk/*
@@ -18,29 +18,48 @@
             pluginspage: "http://www.videolan.org",
             width: "640",
             height: "360",
-            target: video.attr("href"),
+            target: video.attr("href")
         });
         return vlc;
     }
 
     function wf2ch_setclickfunction(object){
     	object.click(function(){
-        	if(object.attr('href') !== ''){
+            console.log(object.attr('wf2ch'));
+
+            if(object.attr('wf2ch') == 1){
+                object.removeAttr("wf2ch");
+                return;
+            }
+
+        	if(object.attr('wf2ch') === undefined){
                 var thumb = object.parent().find('img');
                 thumb.hide();
+
                 var vlc = createVLC(object);
                 thumb.before(vlc);
-                object.attr('href', '');
                 $('#fullscreen-container').hide();
+
+                var closeSpan = $("<span class='collapseWebm'>-[<a>close</a>]</span>");
+                var closeLink = $("a", closeSpan);
+                thumb.before(closeSpan);
+                closeLink.click(function() {
+                    closeSpan.remove();
+                    vlc.remove();
+                    thumb.show();
+                    object.attr("wf2ch", 1);
+                });
+
+                object.attr("wf2ch", 0);
             }
-        });	
+        });
     }
 
     $('a[href$=".webm"]').each(function() {
-    	wf2ch_setclickfunction($(this));	    
+    	wf2ch_setclickfunction($(this));
     });
 
     $('a[href$=".mp4"]').each(function() {
-    	wf2ch_setclickfunction($(this));	    
+    	wf2ch_setclickfunction($(this));
     });
 })();
